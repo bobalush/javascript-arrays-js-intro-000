ea0241cc3f46849c52046cb86b0a6712cb040447
LOI DINH <loiphatdinh@gmail.com>
2018-03-23 00:59:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,8 @@ function addElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift;
+  return array;
+}
