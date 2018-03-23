27d8aa44a498054890f06db9ff9b0366b914d0f5
LOI DINH <loiphatdinh@gmail.com>
2018-03-23 01:07:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,5 +42,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(array.length - 1);
+  array.slice(0, array.length - 1);
 }
